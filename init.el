3b8b66cae02bb274de02f2db5dddcf4897db9497
Jacob MacDonald <jaccarmac@gmail.com>
2015-02-02 20:31:54 -0700

    Install markdown-mode.

--- init.el
+++ init.el
@@ -76,3 +76,17 @@
 
 (quse-package ac-nim :init (eval-after-load 'nim-mode
                              '(add-hook 'nim-mode-hook 'ac-nim-enable)))
+
+(quse-package markdown-mode)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (markdown-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
