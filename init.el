d5354b399ff7e31db1243fd9843908a214b17054
Jacob MacDonald <jaccarmac@gmail.com>
2016-04-26 14:37:48 -0600

    Fix outdated ElDoc setup.
    Seriously, how did I miss this? :-D
    
    https://github.com/clojure-emacs/cider/commit/1b1bb3cc69ed5d52d5a7c138ec07256fd86df83a

--- init.el
+++ init.el
@@ -94,7 +94,7 @@
 
 (quse-package cider
               :init
-              (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+              (add-hook 'cider-mode-hook 'eldoc-mode)
               (setf cider-repl-tab-command 'indent-for-tab-command)
               (setf cider-default-repl-command "boot"))
 
