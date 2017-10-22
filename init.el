fbe4d825747657486828ac37110290a35c6e5caf
jaccarmac <jaccarmac@gmail.com>
2017-10-22 21:36:47 +0000

    Non-destructively set font.
    FossilOrigin-Name: 4c759d1a0cc4b15da25fdaaf243ec2ca88b24c04a317f50872867826e6e7fff4

--- init.el
+++ init.el
@@ -56,7 +56,7 @@
 
 (quse-package rainbow-delimiters)
 
-(setf default-frame-alist '((font . "Go Mono 10")))
+(add-to-list 'default-frame-alist '((font . "Go Mono 10")))
 
 (quse-package emojify :config (global-emojify-mode))
 
